e9601babc8183e6bd1f95317d0933b40bd9acda0
MarielyLopez <mariielylopez795@gmail.com>
2016-05-02 19:57:53 -0600

    first commit

--- js/script.js
+++ js/script.js
@@ -10,6 +10,10 @@ $(document).ready(function () {
   $(this).load("_posts/posts.json", function(data){
     var posts = JSON.parse(data);
     $(".posts").html(posts['posts'][0]['title']);
+    $(".posts2").html(posts['posts'][0]['date']);
+    $(".posts3").html(posts['posts'][0]['date']);
+    $(".posts4").html(posts['posts'][0]['date']);
+    $(".posts5").html(posts['posts'][0]['date']);
     //
   });
 });
